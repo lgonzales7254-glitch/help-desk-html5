52eb65cf53c18559f58344d1d470a4630bb12656
Tu Nombre <tu_correo@example.com>
2026-08-08 20:17:26 -0500

    feat: frontend integrado, seguridad XSS y preparacion para despliegue

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); // 1. Nueva línea para leer las variables de la nube
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -9,14 +10,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Conexión local a MongoDB Compass
-mongoose.connect('mongodb://127.0.0.1:27017/helpdesk_db')
-    .then(() => console.log('¡Conexión exitosa a MongoDB! 🚀'))
-    .catch(err => console.error('Error de conexión a la Base de Datos:', err));
+// 2. Conexión dinámica: Usa la nube si existe, si no, usa tu base local
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/helpdesk_db';
 
-    // Rutas de comunicación de la API REST
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('Conexión exitosa a la Base de Datos! 🚀'))
+  .catch(err => console.error('Error de conexión a la Base de Datos:', err));
+
+// Rutas de comunicación de la API REST (Tus rutas originales)
 app.use('/tickets', ticketRoutes);
 app.use('/auth', authRoutes);
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Servidor Backend ejecutándose en http://localhost:${PORT}`));
+// 3. Puerto dinámico requerido para que Render pueda asignar su propio puerto
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
